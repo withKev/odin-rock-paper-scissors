b2284a6cdc1ec9ad4e585cb7153f91210fbbf60b
Kevin Truong <contact@kevintruong.ca>
2023-09-16 21:57:22 -0700

    Added logic for responsive UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,6 +2,7 @@ let playerSelection = null;
 let computerSelection = null;
 let playerScore = 0;
 let computerScore = 0;
+let gameOver = false;
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3); //Randomly choose 0, 1 or 2
@@ -13,14 +14,24 @@ function getComputerChoice() {
   } else {
     computerSelection = "scissors";
   }
-
+  console.log("Computer chose: " + computerSelection);
   return computerSelection;
 }
 
-function getPlayerChoice() {
-  let userInput = prompt("Rock, Paper or Scissors?");
-  playerSelection = userInput.toLowerCase();
+function getPlayerChoice(input) {
+  playerSelection = input.toLowerCase();
+  console.log("You chose: " + playerSelection);
   return playerSelection;
+}
+
+function updateScore(winner) {
+  if (winner === "user") {
+    playerScore++;
+    return playerScore;
+  } else if (winner === "computer") {
+    computerScore++;
+    return computerScore;
+  } else return;
 }
 
 function getResult() {
@@ -29,54 +40,78 @@ function getResult() {
   let result = "";
 
   if (a === b) {
-    return "It's a tie!";
+    result = "It's a tie!";
   } else if (a === "rock" && b === "scissors") {
-    playerScore++;
+    updateScore("user");
     result = "You won! Rock beats Scissors!";
   } else if (a === "paper" && b === "rock") {
-    playerScore++;
+    updateScore("user");
     result = "You won! Paper beats Rock!";
   } else if (a === "scissors" && b === "paper") {
-    playerScore++;
+    updateScore("user");
     result = "You won! Scissors beats Paper!";
   } else if (a === "rock" && b === "paper") {
-    computerScore++;
-    result = "Aw too bad you lost! Paper beats Rock!";
+    updateScore("computer");
+    result = "Aw too bad you lost! Computer chose Paper!";
   } else if (a === "paper" && b === "scissors") {
-    computerScore++;
-    result = "Aw too bad you lost! Scissors beats Paper!";
+    updateScore("computer");
+    result = "Aw too bad you lost! Computer chose Scissors!";
   } else if (a === "scissors" && b === "rock") {
-    computerScore++;
-    result = "Aw too bad you lost! Rock beats Scissors!";
+    updateScore("computer");
+    result = "Aw too bad you lost! Computer chose Rock!";
   } else {
     result = "Please choose Rock, Paper or Scissors";
   }
-
+  console.log(result);
   return result;
 }
 
-function game(numberOfGames) {
-  for (let i = 1; i <= numberOfGames; i++) {
-    getComputerChoice();
-    getPlayerChoice();
-    console.log("Game " + i + " result: " + getResult());
-    console.log("Score: " + playerScore + " - " + computerScore);
-  }
-
+function getFirstToFive() {
   let result = "";
 
-  if (playerScore === computerScore) {
-    result = "It's a tie! Play again!";
-  } else if (playerScore > computerScore) {
+  if (playerScore >= 1) {
     result = "You won against the computer!";
-  } else if (playerScore < computerScore) {
+    gameOver = true;
+    console.log(result);
+    return result;
+  } else if (computerScore >= 1) {
     result = "You lost against the computer! Better luck next time.";
-  }
+    gameOver = true;
+    console.log(result);
+    return result;
+  } else return (gameOver = false);
+}
 
-  console.log(result);
-  console.log("Final score: " + playerScore + "(You)" + " - " + computerScore);
+function showPlayAgainButton() {
+  if (gameOver === true) {
+    addPlayAgainButton();
+  } else return (gameOver = false);
+}
 
-  return result;
+function resetGameValues() {
+  playerSelection = null;
+  computerSelection = null;
+  playerScore = 0;
+  computerScore = 0;
+  gameOver = false;
+  return "Done.";
+}
+
+function clearOutput() {}
+
+function newGame() {
+  resetGameValues();
+  removePlayAgainButton();
+  // clearOutput(); // should reset the output box of all values
+}
+
+function playGame(choice) {
+  getComputerChoice();
+  playerSelection = choice;
+  getResult();
+  getFirstToFive();
+  showPlayAgainButton();
+  console.log("Score: " + playerScore + " - " + computerScore);
 }
 
 const userChoices = ["Rock", "Paper", "Scissors"];
@@ -85,6 +120,12 @@ const content = document.querySelector(".content");
 function createDiv(choice) {
   const div = document.createElement("div");
   div.classList.add(choice.toLowerCase());
+  div.dataset.choice = choice;
+  div.addEventListener("click", () => {
+    const clickedChoice = div.dataset.choice;
+    // playGame(clickedChoice.toLowerCase());
+    getPlayerChoice(clickedChoice);
+  });
   div.textContent = choice;
   content.appendChild(div);
 }
@@ -93,17 +134,29 @@ userChoices.forEach((choice) => {
   createDiv(choice);
 });
 
-let gameOver = false; // if gameOver = true, show playAgain button
+const confirmButton = document.querySelector(".confirm");
+confirmButton.addEventListener("click", () => {
+  playGame(playerSelection);
+  playerSelection = null;
+});
+
+let playAgainButton;
 
 function addPlayAgainButton() {
   const buttons = document.querySelector(".buttons");
-  const playAgainButton = document.createElement("button");
+  playAgainButton = document.createElement("button");
   playAgainButton.classList.add("playAgain");
   playAgainButton.textContent = "Play Again";
+  playAgainButton.addEventListener("click", () => {
+    newGame();
+  });
 
   buttons.appendChild(playAgainButton);
 }
 
-if (gameOver === true) {
-  addPlayAgainButton();
+function removePlayAgainButton() {
+  const buttons = document.querySelector(".buttons");
+  if (playAgainButton) {
+    buttons.removeChild(playAgainButton);
+  }
 }
